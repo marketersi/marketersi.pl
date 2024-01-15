49e269720f7535899ea50954ab676fdfa61e801a
chiranjivDev <chiranjiv.mohanty@resiliencesoft.com>
2024-01-15 18:56:21 +0530

    feat(zamow):custom accordion added

--- src/components/organisms/zamow/accordion/Accordion.tsx
+++ src/components/organisms/zamow/accordion/Accordion.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { accordionData } from "./data";
+import SingleQuestion from "./SingleQuestion";
+import "./accordion.css";
 
 const Accordion = () => {
-  return <div>Zammow Accordion</div>;
+  const [questions, setQuestions] = useState(accordionData);
+
+  return (
+    <div className="container mb-5">
+      <h3 className="accordion-heading">Odpowiedzi na częste pytania:</h3>
+      <section className="accordion-container w-75">
+        {questions.map((question) => {
+          return (
+            <SingleQuestion key={question.id} {...question}></SingleQuestion>
+          );
+        })}
+      </section>
+    </div>
+  );
 };
 
 export default Accordion;
