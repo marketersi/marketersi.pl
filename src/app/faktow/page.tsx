0e44c510023d9723f71cf3c76567416c019d2d5f
chiranjivDev <chiranjiv.mohanty@resiliencesoft.com>
2024-01-17 11:57:17 +0530

    feat(facts): initial UI

---
+++ src/app/faktow/page.tsx
@@ -0,0 +1,14 @@
+import FactsContainer from "@/components/organisms/facts/facts-container/factsContainer";
+import Hero from "@/components/organisms/facts/hero/Hero";
+import React from "react";
+
+const FactsOfPsychology = () => {
+  return (
+    <>
+      <Hero />
+      <FactsContainer />
+    </>
+  );
+};
+
+export default FactsOfPsychology;
