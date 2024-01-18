3d9deba7f2b4490acff7c67d15a07b36873932b9
chiranjivDev <chiranjiv.mohanty@resiliencesoft.com>
2024-01-18 11:45:21 +0530

    feat(zamow):Back To Top button added

--- src/app/zamow/page.tsx
+++ src/app/zamow/page.tsx
@@ -1,10 +1,11 @@
+"use client";
 import React from "react";
 import ZamowHero from "@/components/organisms/zamow/zamow-hero/ZamowHero";
-import "./zamow.css";
 import CustomerRating from "@/components/organisms/zamow/customer-rating/CustomerRating";
 import Accordion from "@/components/organisms/zamow/accordion/Accordion";
 import { Container } from "react-bootstrap";
 import Results from "@/components/organisms/zamow/results/Results";
+import "./zamow.css";
 
 const BookExamination = () => {
   return (
@@ -14,6 +15,14 @@ const BookExamination = () => {
         <CustomerRating />
         <Results />
         <Accordion />
+        <div className="btt_btn_container">
+          <button
+            onClick={() => console.log("back to top")}
+            className="btt_btn"
+          >
+            Powrót na górę
+          </button>
+        </div>
       </Container>
     </>
   );
