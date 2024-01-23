94834d2316247be6be10545a16be6c6cf5a25143
chiranjivDev <chiranjiv.mohanty@resiliencesoft.com>
2024-01-23 17:43:46 +0530

    feat(zamow): 13 questions components added

---
+++ src/components/organisms/zamow/zamow-hero/questions/Question3.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Question3 = ({ handleNext }) => {
+  return (
+    <div className="zh_question">
+      <h2>Jak masz na imię? 3</h2>
+      <input />
+      <div className="zh_next_btn_container">
+        <motion.button
+          onClick={handleNext}
+          className="zh_next_btn"
+          whileHover={{ translateY: 5 }}
+        >
+          OK
+        </motion.button>
+      </div>
+    </div>
+  );
+};
+
+export default Question3;
--- src/components/organisms/zamow/zamow-hero/questions/Questions.tsx
+++ src/components/organisms/zamow/zamow-hero/questions/Questions.tsx
@@ -1,23 +1,70 @@
 import React from "react";
-import "./questions.css";
 import { motion } from "framer-motion";
+import "./questions.css";
 
-const Questions = ({ handleNext }) => {
-  return (
-    <div className="zh_question">
-      <h2>Jak masz na imię?</h2>
-      <input />
-      <div className="zh_next_btn_container">
-        <motion.button
-          onClick={handleNext}
-          className="zh_next_btn"
-          whileHover={{ translateY: 5 }}
-        >
-          OK
-        </motion.button>
-      </div>
-    </div>
-  );
+// Import all your question components
+import Question1 from "./Question1";
+import Question2 from "./Question2";
+import Question3 from "./Question3";
+import Question4 from "./Question4";
+import Question5 from "./Question5";
+import Question6 from "./Question6";
+import Question7 from "./Question7";
+import Question8 from "./Question8";
+import Question9 from "./Question9";
+import Question10 from "./Question10";
+import Question11 from "./Question11";
+import Question12 from "./Question12";
+import Question13 from "./Question13";
+
+const Questions = ({ handleNext, currentQuestion }) => {
+  let questionComponent;
+
+  switch (currentQuestion) {
+    case 1:
+      questionComponent = <Question1 handleNext={handleNext} />;
+      break;
+    case 2:
+      questionComponent = <Question2 handleNext={handleNext} />;
+      break;
+    case 3:
+      questionComponent = <Question3 handleNext={handleNext} />;
+      break;
+    case 4:
+      questionComponent = <Question4 handleNext={handleNext} />;
+      break;
+    case 5:
+      questionComponent = <Question5 handleNext={handleNext} />;
+      break;
+    case 6:
+      questionComponent = <Question6 handleNext={handleNext} />;
+      break;
+    case 7:
+      questionComponent = <Question7 handleNext={handleNext} />;
+      break;
+    case 8:
+      questionComponent = <Question8 handleNext={handleNext} />;
+      break;
+    case 9:
+      questionComponent = <Question9 handleNext={handleNext} />;
+      break;
+    case 10:
+      questionComponent = <Question10 handleNext={handleNext} />;
+      break;
+    case 11:
+      questionComponent = <Question11 handleNext={handleNext} />;
+      break;
+    case 12:
+      questionComponent = <Question12 handleNext={handleNext} />;
+      break;
+    case 13:
+      questionComponent = <Question13 handleNext={handleNext} />;
+      break;
+    default:
+      questionComponent = <div className="zh_question">No more questions</div>;
+  }
+
+  return <>{questionComponent}</>;
 };
 
 export default Questions;
