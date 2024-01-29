be991e24ebfed374b82cd0e96932124a7c15345d
chiranjivDev <chiranjiv.mohanty@resiliencesoft.com>
2024-01-29 18:39:22 +0530

    feat(cennik): cardone slider dynamic para

--- src/components/organisms/cennik/CardOne.tsx
+++ src/components/organisms/cennik/CardOne.tsx
@@ -142,11 +142,45 @@ export default CardOne;
 
 // Nested components for card one
 const SliderSection = ({ setCurrentComponent }) => {
+  const [currentSection, setCurrentSection] = useState(0);
+
+  const sectionDescriptions = [
+    {
+      main: "Przesuń suwak blisko potrzeb",
+      additional: "",
+    },
+    {
+      main: "Strategiczna - Duża marka",
+      additional: "programista + projektant UX",
+    },
+    {
+      main: "Wiodąca - Pod marketing.",
+      additional: "programista + projektant UX",
+    },
+    {
+      main: "Klasyczna - Firmowa. Fajna.",
+      additional: "programista + projektant ",
+    },
+    {
+      main: "Najtańsza – bardzo prosta",
+      additional: "programista + projektant",
+    },
+  ];
+
+  const totalSections = sectionDescriptions.length;
+
+  const handleSliderChange = (e) => {
+    // Calculate the current section based on the slider value
+    const sliderValue = e.target.value;
+    const calculatedSection = Math.floor((sliderValue / 100) * totalSections);
+    setCurrentSection(calculatedSection);
+  };
   return (
     <div className="slider_section">
       <div>
         <h2>Jakiej propozycji oczekujesz?</h2>
-        <p>Przesuń suwak blisko potrzeb</p>
+        <p>{sectionDescriptions[currentSection]?.main}</p>
+        <div>{sectionDescriptions[currentSection]?.additional}</div>
       </div>
 
       <div className="space-between"></div>
@@ -156,7 +190,11 @@ const SliderSection = ({ setCurrentComponent }) => {
             src="https://www.owocni.pl/_next/static/media/persons.6bff2827.png"
             alt=""
           />
-          <input type="range" />
+          <input
+            type="range"
+            onChange={handleSliderChange}
+            value={(currentSection / (totalSections - 1)) * 100}
+          />
           <img
             src="https://www.owocni.pl/_next/static/media/person.a15d0f85.png"
             alt=""
