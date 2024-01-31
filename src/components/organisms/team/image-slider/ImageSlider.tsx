20e4f6f1b5823fe9c7bf76e9d00aad1bc5f5fdfc
premsahu88 <103987763+premsahu88@users.noreply.github.com>
2024-01-31 15:49:06 +0530

    zespol screen animation and functionalty

--- src/components/organisms/team/image-slider/ImageSlider.tsx
+++ src/components/organisms/team/image-slider/ImageSlider.tsx
@@ -1,14 +1,18 @@
-"use client";
 
+import ImageSlider from './CarouselImage';
+import {motion, Variants} from 'framer-motion'
 import React from "react";
 import style from "../team.module.css";
-import Carousel from "react-bootstrap/Carousel";
-import ExampleCarouselImage from "./CarouselImageSlider";
-import "./imageslider.css";
-import { motion, Variants } from "framer-motion";
-import Image from "next/image";
 
-const ImageSlider = () => {
+
+const images = [
+    'https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F1.95c40610.jpg&w=1920&q=75',
+    'https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F3.b7a94977.jpg&w=1920&q=75',
+    'https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F2.aa0277f1.jpg&w=1920&q=75',
+   
+  ];
+
+const Home = () => {
 
   const springUp: Variants = {
     offscreen: {
@@ -25,99 +29,10 @@ const ImageSlider = () => {
       },
     },
   };
-
-  
-  // const images = [
-  //   'https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F1.95c40610.jpg&w=1920&q=75',
-  //   'https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F3.b7a94977.jpg&w=1920&q=75',
-  //   'https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F2.aa0277f1.jpg&w=1920&q=75',
-   
-  // ];
-
   return (
-    <>
-      <section className={style.imageSlider}>
-        <Carousel indicators={false} className="image-slider">
-          <Carousel.Item>
-            <ExampleCarouselImage
-              src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F1.95c40610.jpg&w=1920&q=75"
-              alt="Second slide"
-            />
-            <Carousel.Caption></Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <ExampleCarouselImage
-              src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F3.b7a94977.jpg&w=1920&q=75"
-              alt="Second slide"
-            />
-            <Carousel.Caption></Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <ExampleCarouselImage
-              src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F2.aa0277f1.jpg&w=1920&q=75"
-              alt="Second slide"
-            />
-            <Carousel.Caption></Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <ExampleCarouselImage
-              src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F4.607a7992.jpg&w=1920&q=75"
-              alt="Second slide"
-            />
-            <Carousel.Caption></Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <ExampleCarouselImage
-              src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F5.67f20c73.jpg&w=1920&q=75"
-              alt="Second slide"
-            />
-            <Carousel.Caption></Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <ExampleCarouselImage
-              src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F6.16146c68.jpg&w=1920&q=75"
-              alt="Second slide"
-            />
-            <Carousel.Caption></Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <ExampleCarouselImage
-              src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F3.b7a94977.jpg&w=1920&q=75"
-              alt="Second slide"
-            />
-            <Carousel.Caption></Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <ExampleCarouselImage
-              src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F8.e6d0e7e3.jpg&w=1920&q=75"
-              alt="Second slide"
-            />
-            <Carousel.Caption></Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <ExampleCarouselImage
-              src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F9.1e9d84fe.jpg&w=1920&q=75"
-              alt="Second slide"
-            />
-            <Carousel.Caption></Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <ExampleCarouselImage
-              src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F10.8c3cf7dd.jpg&w=1920&q=75"
-              alt="Second slide"
-            />
-            <Carousel.Caption></Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <ExampleCarouselImage
-              src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F11.350adefb.jpg&w=1920&q=75"
-              alt="Second slide"
-            />
-            <Carousel.Caption></Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-        {/* <ImageSlider images={images} /> */}
-        <motion.img
+    <div>
+      <ImageSlider images={images} />
+      <motion.img
           className={style.slideAnimateImg}
           src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FOwocni-Film.4c8c215f.png&w=640&q=75"
           alt=""
@@ -135,11 +50,8 @@ const ImageSlider = () => {
             odpowiedzieli:
           </p>
         </div>
-      </section>
-    </>
+    </div>
   );
 };
 
-
-
-export default ImageSlider;
+export default Home;
