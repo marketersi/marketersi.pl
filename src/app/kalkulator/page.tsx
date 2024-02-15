4aa03d177390ff7dd5cb567b0d79e8bdd2b15748
premsahu88 <103987763+premsahu88@users.noreply.github.com>
2024-02-15 15:04:24 +0530

    Calculator screen import fix

--- src/app/kalkulator/page.tsx
+++ src/app/kalkulator/page.tsx
@@ -5,9 +5,8 @@ import Calculator from "@/components/organisms/calculator/Calculator";
 import { useDispatch, useSelector } from "react-redux";
 import { FETCH_CALCULATOR_SCREEN_DATA } from "@/redux/kalkulator/calculatorAction";
 import Loader from "@/components/organisms/animation/Loader";
+import Warranty from "@/components/organisms/calculator/Warranty";
 import "./calculator.css";
-import { Fira_Code } from "next/font/google";
-import Warranty from "@/components/organisms/calculator/warranty";
 
 const CalculatorScreen = () => {
   const { isLoading, screenData } = useSelector((state) => state.calculator);
@@ -31,14 +30,14 @@ const CalculatorScreen = () => {
             <Row className="first-row">
               <Col className="text-center">
                 <h1>
-                  {screenData?.first_heading && screenData?.first_heading.split(" ").slice(0, 3).join(" ")}
+                  {screenData?.first_heading &&
+                    screenData?.first_heading.split(" ").slice(0, 3).join(" ")}
                 </h1>
                 <h1>
-                  {screenData?.first_heading && screenData?.first_heading.split(" ").slice(3).join(" ")}
+                  {screenData?.first_heading &&
+                    screenData?.first_heading.split(" ").slice(3).join(" ")}
                 </h1>
-                <p>
-                  {screenData?.sub_heading}
-                </p>
+                <p>{screenData?.sub_heading}</p>
               </Col>
             </Row>
 
@@ -47,16 +46,19 @@ const CalculatorScreen = () => {
             <Row className="second-row">
               <Col className="text-center p-sm-5 p-4">
                 <h1>
-                  {screenData?.second_heading && screenData?.second_heading.split(" ").slice(0, 4).join(" ")}
-                  <span>{screenData?.second_heading && screenData?.second_heading.split(" ").slice(4).join(" ")}</span>
+                  {screenData?.second_heading &&
+                    screenData?.second_heading.split(" ").slice(0, 4).join(" ")}
+                  <span>
+                    {screenData?.second_heading &&
+                      screenData?.second_heading.split(" ").slice(4).join(" ")}
+                  </span>
                 </h1>
-                <div className="calculator_container" style={{ margin: "auto" }}>
-                  <p className="mt-5">
-                    {screenData?.first_paragraph}
-                  </p>
-                  <p>
-                    {screenData?.second_paragraph}
-                  </p>
+                <div
+                  className="calculator_container"
+                  style={{ margin: "auto" }}
+                >
+                  <p className="mt-5">{screenData?.first_paragraph}</p>
+                  <p>{screenData?.second_paragraph}</p>
                 </div>
               </Col>
             </Row>
