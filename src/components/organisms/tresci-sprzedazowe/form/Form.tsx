00a6e8a701f7a791694f6f5f75a18cf25aefd213
chiranjivDev <chiranjiv.mohanty@resiliencesoft.com>
2024-03-14 19:13:25 +0530

    Get A Quote Form UI initial

--- src/components/organisms/tresci-sprzedazowe/form/Form.tsx
+++ src/components/organisms/tresci-sprzedazowe/form/Form.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "../tresci.css";
 import { SEND_ME_A_QUOTE } from "@/redux/tresci/salescontentAction";
+import { useRouter } from "next/navigation";
 
 const Form = () => {
   const {
@@ -13,7 +14,10 @@ const Form = () => {
     formState: { errors },
   } = useForm();
 
+  const { isFormSubmitted } = useSelector((state) => state.sales);
+
   const dispatch = useDispatch();
+  const router = useRouter();
 
   const onSubmit = (data) => {
     console.log(data);
@@ -21,33 +25,52 @@ const Form = () => {
     reset();
   };
 
+  useEffect(() => {
+    if (isFormSubmitted) {
+      router.push("/dziekujemy");
+    }
+  }, [isFormSubmitted, router]);
+
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <input
-            required
-            placeholder="Imię"
-            {...register("name", { required: true })}
-          />
-          <input
-            required
-            placeholder="Mail"
-            type="email"
-            {...register("email", { required: true })}
-          />
-          <input
-            placeholder="Telefon"
-            pattern="\d{7,15}"
-            {...register("phone")}
-          />
-          <textarea {...register("message")} />
-
-          <div>
-            <button type="submit">send a quote</button>
+      <div className="sendQuote">
+        <h2>
+          Napisz kilka słów o swoim projekcie i otrzymaj niezobowiązującą
+          ofertę.
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="inputLayout">
+            <input
+              required
+              placeholder="Imię"
+              {...register("name", { required: true })}
+            />
+            <input
+              required
+              placeholder="Mail"
+              type="email"
+              {...register("email", { required: true })}
+            />
+            <input
+              placeholder="Tel."
+              pattern="\d{7,15}"
+              {...register("phone")}
+            />
           </div>
-        </div>
-      </form>
+          <textarea
+            {...register("message")}
+            placeholder="Napisz kilka słów o swoim projekcie"
+          />
+
+          <div className="quoteBtn">
+            <button type="submit">Wyślijcie mi niezobowiązującą ofertę</button>
+            <img
+              src="https://images.prismic.io/marketersi/ZfL0JUmNsf2sHk-N_Group1.png?auto=format,compress"
+              alt=""
+            />
+          </div>
+        </form>
+      </div>
     </>
   );
 };
