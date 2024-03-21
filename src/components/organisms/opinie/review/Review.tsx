13dc90631809a7c4fb783f94eb87ab1fbb0c7c3d
chiranjivDev <chiranjiv.mohanty@resiliencesoft.com>
2024-03-21 15:37:11 +0530

    feat(opinion): Review section data mapping and UI

--- src/components/organisms/opinie/review/Review.tsx
+++ src/components/organisms/opinie/review/Review.tsx
@@ -1,220 +1,63 @@
 import React from "react";
 import style from "../opinie.module.css";
-import { Row, Col } from "react-bootstrap";
-import {
-  ClientCarousel,
-  CarouselContext,
-} from "../../strategia-marketingowa/ClientCarouselNext";
-import { useContext } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { useSelector } from "react-redux";
+import Stars from "@/components/molecules/Ratings";
 
-const SlideOne = () => {
-  const { handleNext } = useContext(CarouselContext);
-  return (
-    <>
-<div className={style.opinionContainer}>
-          <Row>
-            <Col sm={4}>
-              <div className={style.cardReview}>
-                <img
-                  className={style.reviewMainImg}
-                  src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F5a.f65a0a00.png&w=384&q=75"
-                  alt=""
-                />
-                <img
-                  className={style.reviewLogoImg}
-                  src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F5b.13bba4d4.png&w=256&q=75"
-                  alt=""
-                />
-                <div className={style.star}>
-                  <img
-                    src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fstar.c3a29cce.jpg&w=128&q=75"
-                    alt=""
-                  />
-                  <div>
-                    <h6>Anna i Maciej Dobosz</h6>
-                    <p>stomatologia</p>
-                  </div>
-                </div>
-                <p>
-                    „Owocni zrobili nam takie projekty, jakie chcieliśmy mieć,{" "}
-                    <span>i jakie podobają się</span> naszym pacjentom. Teraz to
-                    my jesteśmy 'tą firmą od ładnej strony'.”
-                  </p>
-              </div>
-            </Col>
-            <Col sm={4}>
-              <div className={style.cardReview}>
-                <img
-                  className={style.reviewMainImg}
-                  src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F5a.f65a0a00.png&w=384&q=75"
-                  alt=""
-                />
-                <img
-                  className={style.reviewLogoImg}
-                  src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F5b.13bba4d4.png&w=256&q=75"
-                  alt=""
-                />
-                <div className={style.star}>
-                  <img
-                    src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fstar.c3a29cce.jpg&w=128&q=75"
-                    alt=""
-                  />
-                  <div>
-                    <h6>Anna i Maciej Dobosz</h6>
-                    <p>stomatologia</p>
-                  </div>
-                </div>
-                <p>
-                    „Owocni zrobili nam takie projekty, jakie chcieliśmy mieć,{" "}
-                    <span>i jakie podobają się</span> naszym pacjentom. Teraz to
-                    my jesteśmy 'tą firmą od ładnej strony'.”
-                  </p>
-              </div>
-            </Col>
-            <Col sm={4}>
-              <div className={style.cardReview}>
-                <img
-                  className={style.reviewMainImg}
-                  src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F5a.f65a0a00.png&w=384&q=75"
-                  alt=""
-                />
-                <img
-                  className={style.reviewLogoImg}
-                  src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F5b.13bba4d4.png&w=256&q=75"
-                  alt=""
-                />
-                <div className={style.star}>
-                  <img
-                    src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fstar.c3a29cce.jpg&w=128&q=75"
-                    alt=""
-                  />
-                  <div>
-                    <h6>Anna i Maciej Dobosz</h6>
-                    <p>stomatologia</p>
-                  </div>
-                </div>
-                <p>
-                    „Owocni zrobili nam takie projekty, jakie chcieliśmy mieć,{" "}
-                    <span>i jakie podobają się</span> naszym pacjentom. Teraz to
-                    my jesteśmy 'tą firmą od ładnej strony'.”
-                  </p>
-              </div>
-            </Col>
-          </Row>
-        </div>
-    </>
-  )
-}
-const SlideTwo = () => {
-  const { handleNext } = useContext(CarouselContext);
-  return (
-    <>
-<div className={style.opinionContainer}>
-          <Row>
-            <Col sm={4}>
-              <div className={style.cardReview}>
-                <img
-                  className={style.reviewMainImg}
-                  src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F5a.f65a0a00.png&w=384&q=75"
-                  alt=""
-                />
-                <img
-                  className={style.reviewLogoImg}
-                  src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F5b.13bba4d4.png&w=256&q=75"
-                  alt=""
-                />
-                <div className={style.star}>
-                  <img
-                    src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fstar.c3a29cce.jpg&w=128&q=75"
-                    alt=""
-                  />
-                  <div>
-                    <h6>Anna i Maciej Dobosz</h6>
-                    <p>stomatologia</p>
-                  </div>
-                </div>
-                <p>
-                    „Owocni zrobili nam takie projekty, jakie chcieliśmy mieć,{" "}
-                    <span>i jakie podobają się</span> naszym pacjentom. Teraz to
-                    my jesteśmy 'tą firmą od ładnej strony'.”
-                  </p>
-              </div>
-            </Col>
-            <Col sm={4}>
-              <div className={style.cardReview}>
-                <img
-                  className={style.reviewMainImg}
-                  src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F5a.f65a0a00.png&w=384&q=75"
-                  alt=""
-                />
-                <img
-                  className={style.reviewLogoImg}
-                  src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F5b.13bba4d4.png&w=256&q=75"
-                  alt=""
-                />
-                <div className={style.star}>
-                  <img
-                    src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fstar.c3a29cce.jpg&w=128&q=75"
-                    alt=""
-                  />
-                  <div>
-                    <h6>Anna i Maciej Dobosz</h6>
-                    <p>stomatologia</p>
-                  </div>
-                </div>
-                <p>
-                    „Owocni zrobili nam takie projekty, jakie chcieliśmy mieć,{" "}
-                    <span>i jakie podobają się</span> naszym pacjentom. Teraz to
-                    my jesteśmy 'tą firmą od ładnej strony'.”
-                  </p>
-              </div>
-            </Col>
-            <Col sm={4}>
-              <div className={style.cardReview}>
-                <img
-                  className={style.reviewMainImg}
-                  src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F5a.f65a0a00.png&w=384&q=75"
-                  alt=""
-                />
-                <img
-                  className={style.reviewLogoImg}
-                  src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F5b.13bba4d4.png&w=256&q=75"
-                  alt=""
-                />
-                <div className={style.star}>
-                  <img
-                    src="https://owocni.pl/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fstar.c3a29cce.jpg&w=128&q=75"
-                    alt=""
-                  />
-                  <div>
-                    <h6>Anna i Maciej Dobosz</h6>
-                    <p>stomatologia</p>
-                  </div>
-                </div>
-                <p>
-                    „Owocni zrobili nam takie projekty, jakie chcieliśmy mieć,{" "}
-                    <span>i jakie podobają się</span> naszym pacjentom. Teraz to
-                    my jesteśmy 'tą firmą od ładnej strony'.”
-                  </p>
-              </div>
-            </Col>
-          </Row>
-        </div>
-    </>
-  )
-}
 export default function Review() {
-  const slides = [SlideOne, SlideTwo];
-  const { handleNext } = useContext(CarouselContext);
+  const { screenData } = useSelector((state) => state.opinion);
+  const Review = screenData.Review || [];
+
+  function SampleNextArrow(props) {
+    const { onClick } = props;
+    return (
+      <div className="brand_Slide_ButtonNext">
+        <div className="OurClients_Slide_Button_Text">Następna</div>
+        <div className="ArrowButton" onMouseEnter={onClick}>
+          <span className="ArrowButton_Arrow">&gt;</span>
+        </div>
+      </div>
+    );
+  }
+
+  var settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    nextArrow: <SampleNextArrow />,
+  };
+
   return (
     <>
       <div className={style.job}>
-        <ClientCarousel slides={slides} options={{loop: true}} />
+        <div className={style.opinionContainer}>
+          <Slider {...settings}>
+            {Review.map((e, i) => {
+              return <ReviewCard data={e} key={i} />;
+            })}
+          </Slider>
+        </div>
       </div>
-      
-      
     </>
   );
 }
 
-// export default Review;
+const ReviewCard = ({ data }) => {
+  return (
+    <div className={style.cardReview}>
+      <img className={style.reviewMainImg} src={data.client_image} alt="" />
+      <img className={style.reviewLogoImg} src={data.client_logo} alt="" />
+      <div className={style.star}>
+        <Stars rating={data.rating_no} size="16px" noShadow />
+        <div>
+          <h6>{data.client_name}</h6>
+          <p>{data.designation}</p>
+        </div>
+      </div>
+      <p>{data.paragraph}</p>
+    </div>
+  );
+};
