a3953c30f2ff51acd36cafbab650a26d961178a6
chiranjivDev <chiranjiv.mohanty@resiliencesoft.com>
2024-03-26 15:25:46 +0530

    feat(bug): stategia initial bug fix

--- src/components/organisms/strategia-marketingowa/QuoteOne.tsx
+++ src/components/organisms/strategia-marketingowa/QuoteOne.tsx
@@ -2,23 +2,20 @@ import Image from "next/image";
 import { useSelector } from "react-redux";
 
 export default function QuoteOne() {
-
   const { isLoading, screenData } = useSelector((state) => state.strategy);
-  const {quoteOne } = screenData;
+  const { quoteOne } = screenData;
 
   return (
     <div className="QuoteOne content">
       <div className="QuoteOne_Image full">
-        <Image
+        <img
           src={quoteOne?.image}
-          height={733}
-          width={918}
+          // height={733}
+          // width={918}
           alt="QuoteOneImage"
         />
       </div>
-      <div className="QuoteOne_Text feature">
-        {quoteOne?.bottom_title}
-      </div>
+      <div className="QuoteOne_Text feature">{quoteOne?.bottom_title}</div>
     </div>
   );
 }
