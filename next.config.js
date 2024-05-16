f641215b079bd20d35e6ca2eedca593615f9b15c
premsahu88 <103987763+premsahu88@users.noreply.github.com>
2024-05-16 16:10:51 +0530

    implemeted contact email env variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  env: {
+    SERVICE_ID: process.env.SERVICE_ID,
+    TEMPLATE_ID: process.env.TEMPLATE_ID,
+    PUBLIC_KEY: process.env.PUBLIC_KEY,
+  },
   images: {
     unoptimized: true,
   },
