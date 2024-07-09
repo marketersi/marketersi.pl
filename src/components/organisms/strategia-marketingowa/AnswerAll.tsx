835ab82e3a8c6e5202ec0cdf42159dd5c4b94524
chiranjivDev <chiranjivmohanty@resiliencesoft.com>
2024-07-09 16:53:44 +0530

    feat(strategia): fix img size

--- src/components/organisms/strategia-marketingowa/AnswerAll.tsx
+++ src/components/organisms/strategia-marketingowa/AnswerAll.tsx
@@ -2,22 +2,20 @@ import Image from "next/image";
 import { useSelector } from "react-redux";
 
 export default function AnswerAll() {
-
   const { isLoading, screenData } = useSelector((state) => state.strategy);
   const { FAQsection } = screenData;
 
   return (
     <div className="AnswerAll content">
-      <div className="AnswerAll_Title">
-        {FAQsection?.title}
-      </div>
-      <div className="AnswerAll_Image feature">
+      <div className="AnswerAll_Title">{FAQsection?.title}</div>
+      <div className="AnswerAll_Image feature" style={{ textAlign: "center" }}>
         <Image
           src={FAQsection?.image_url}
           alt="AnswerAllImage"
           height={299}
           width={1001}
           className="yes_accordion_image"
+          style={{ width: "auto" }}
         />
       </div>
     </div>
