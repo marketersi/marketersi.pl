2720300244e1ff317ccd927e7c7e55b29ba7c032
chiranjivDev <chiranjivmohanty@resiliencesoft.com>
2024-07-15 15:56:58 +0530

    zamow screen image upload, add loader in opinie screen

--- src/app/marketersi-opinie/page.tsx
+++ src/app/marketersi-opinie/page.tsx
@@ -1,25 +1,26 @@
-"use client";
-import React, { useEffect } from "react";
-import Banner from "@/components/organisms/opinie/banner/Banner";
-import Review from "@/components/organisms/opinie/review/Review";
-import Teams from "@/components/organisms/opinie/teams/Teams";
-import Brands from "@/components/organisms/opinie/brands/Brands";
-import Rating from "@/components/organisms/opinie/rating/Rating";
-import Accordion from "@/components/organisms/opinie/accordion/Accordion";
-import GoogleReview from "@/components/organisms/opinie/google-review/GoogleReview";
-import References from "@/components/organisms/opinie/references/References";
-import Personally from "@/components/organisms/opinie/personally/Personally";
-import Experts from "@/components/organisms/opinie/expert/Experts";
-import Order from "@/components/organisms/main/order/Order";
-import { useDispatch, useSelector } from "react-redux";
-import { FETCH_OPINION_SCREEN_DATA } from "@/redux/opinie/opinionAction";
-import { FETCH_HOME_SCREEN_DATA } from "@/redux/home/homeAction";
-import "./opinion.css";
-import ReviewCard from "@/components/organisms/opinie/brilliant-job/ReviewCard";
-import HappyCustomers from "@/components/organisms/opinie/happy-customers/HappyCustomers";
+'use client';
+import React, { useEffect } from 'react';
+import Banner from '@/components/organisms/opinie/banner/Banner';
+import Review from '@/components/organisms/opinie/review/Review';
+import Teams from '@/components/organisms/opinie/teams/Teams';
+import Brands from '@/components/organisms/opinie/brands/Brands';
+import Rating from '@/components/organisms/opinie/rating/Rating';
+import Accordion from '@/components/organisms/opinie/accordion/Accordion';
+import GoogleReview from '@/components/organisms/opinie/google-review/GoogleReview';
+import References from '@/components/organisms/opinie/references/References';
+import Personally from '@/components/organisms/opinie/personally/Personally';
+import Experts from '@/components/organisms/opinie/expert/Experts';
+import Order from '@/components/organisms/main/order/Order';
+import { useDispatch, useSelector } from 'react-redux';
+import { FETCH_OPINION_SCREEN_DATA } from '@/redux/opinie/opinionAction';
+import { FETCH_HOME_SCREEN_DATA } from '@/redux/home/homeAction';
+import './opinion.css';
+import ReviewCard from '@/components/organisms/opinie/brilliant-job/ReviewCard';
+import HappyCustomers from '@/components/organisms/opinie/happy-customers/HappyCustomers';
+import BounceLoader from 'react-spinners/BounceLoader';
 
 const CustomersOpinion = () => {
-  const { screenData } = useSelector((state) => state.opinion);
+  const { isLoading, screenData } = useSelector((state) => state.opinion);
   const AccordionOne = screenData.Acoordion1 || {};
   const AccordionTwo = screenData.Acoordion2 || {};
 
@@ -29,6 +30,13 @@ const CustomersOpinion = () => {
     dispatch({ type: FETCH_OPINION_SCREEN_DATA });
     dispatch({ type: FETCH_HOME_SCREEN_DATA });
   }, [dispatch]);
+
+  if (isLoading)
+    return (
+      <div className="loader-container">
+        <BounceLoader color="#00bfff" size={50} />
+      </div>
+    );
 
   return (
     <>
