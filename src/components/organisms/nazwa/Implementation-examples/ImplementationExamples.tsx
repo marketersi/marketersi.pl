ade7974b805846bdfa3a245a98fff744dee2ed51
chiranjivDev <chiranjivmohanty@resiliencesoft.com>
2024-07-30 15:58:32 +0530

    fix points from sheet: for tresi, nazwa and strategia

--- src/components/organisms/nazwa/Implementation-examples/ImplementationExamples.tsx
+++ src/components/organisms/nazwa/Implementation-examples/ImplementationExamples.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import ImplementationExample1 from "./examples/ImplementaionEx";
-import ImplementationExample2 from "./examples/ImplementaionEx2";
-import CustomerReview from "../customer-review/CustomerReview";
-import ImplementationEx3 from "./examples/ImplementationEx3";
-import CustomerReview2 from "../customer-review/CustomerReview2";
-import ImplementationEx4 from "./examples/ImplementationEx4";
-import CustomerReview3 from "../customer-review/CustomerReview3";
-import ImplementationEx5 from "./examples/ImplementationEx5";
-import ImplementationEx6 from "./examples/ImplementationEx6";
-import ImplementationEx7 from "./examples/ImplementationEx7";
-import ImplementationEx8 from "./examples/ImplementationEx8";
-import Profit from "../../tresci-sprzedazowe/profit-slider/Profit";
-import Form from "../../tresci-sprzedazowe/form/Form";
-import "./examples.css";
-import NazwaSlider from "../NazwaSlider";
+import React from 'react';
+import ImplementationExample1 from './examples/ImplementaionEx';
+import ImplementationExample2 from './examples/ImplementaionEx2';
+import CustomerReview from '../customer-review/CustomerReview';
+import ImplementationEx3 from './examples/ImplementationEx3';
+import CustomerReview2 from '../customer-review/CustomerReview2';
+import ImplementationEx4 from './examples/ImplementationEx4';
+import CustomerReview3 from '../customer-review/CustomerReview3';
+import ImplementationEx5 from './examples/ImplementationEx5';
+import ImplementationEx6 from './examples/ImplementationEx6';
+import ImplementationEx7 from './examples/ImplementationEx7';
+import ImplementationEx8 from './examples/ImplementationEx8';
+import Profit from '../../tresci-sprzedazowe/profit-slider/Profit';
+import Form from '../../tresci-sprzedazowe/form/Form';
+import './examples.css';
+import NazwaSlider from '../NazwaSlider';
 
 const ImplementationExamples = () => {
   return (
@@ -47,13 +47,14 @@ const ImplementationExamples = () => {
       <img
         src="https://cdn.owocni.pl/img/single-projektowanie-nazw/luk-black.png"
         className="projects-bow-1 names-bow-bgcolor-purple"
-        style={{ marginTop: "-127px" }}
+        style={{ marginTop: '-127px' }}
         alt=""
       ></img>
 
       <ImplementationEx8 />
 
-      <NazwaSlider />
+      {/* to be deleted */}
+      {/* <NazwaSlider /> */}
 
       <section className="black-sec-form">
         <div className="copywriter-ending-1 text-center">
--- src/components/organisms/tresci-sprzedazowe/options-box2/OptionBox2.tsx
+++ src/components/organisms/tresci-sprzedazowe/options-box2/OptionBox2.tsx
@@ -1,51 +1,60 @@
-import React, { useState } from 'react'
-import '../tresci.css'
+import React, { useState } from 'react';
+import '../tresci.css';
 import { ModalForm } from '../ModalForm';
 
-const OptionBox2 = ({optionBox2 }) => {
-  const { background_image = '', description = '', title = '', btn_title = '' } = optionBox2 || {};
+const OptionBox2 = ({ optionBox2 }) => {
+  const {
+    background_image = '',
+    description = '',
+    title = '',
+    btn_title = '',
+  } = optionBox2 || {};
 
-  const [ isModal, setIsModal] = useState(false)
+  const [isModal, setIsModal] = useState(false);
 
   const handleModalClose = () => {
     setIsModal(!isModal);
   };
   return (
     <div>
-        <section className="projects-gray-opinion-2">
-      <div className="container projects-opinion-box-container">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="opinion-box">
-              <div className="opinion-box-image">
-                <img
-                  className="opinion-box-inside-image"
-                  src={background_image}
-                  title="Usługi copywriterskie dla różnych branż"
-                  alt="Opinia klienta jest bardzo ważna dla agencji marketingowej"
-                />
-              </div>
-              <div className="opinion-box-content projects-opinion-2 copywriter-prawo-text">
-                <p className="theme-desc projects-client-opinion-p text-center">
-                  {title}
-                </p>
-                <div className="opinion-box-text copywriter-opinion-box-text">
-                  {description}
+      <section className="projects-gray-opinion-2">
+        <div className="container projects-opinion-box-container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="opinion-box">
+                <div className="opinion-box-image">
+                  <img
+                    className="opinion-box-inside-image"
+                    src={background_image}
+                    title="Usługi copywriterskie dla różnych branż"
+                    alt="Opinia klienta jest bardzo ważna dla agencji marketingowej"
+                  />
                 </div>
-                <button onClick={() => setIsModal(true)} className="download-catalog-button projects-opinion-button copywriter-green-background copywriter-new-btn copywriter-standard-mt-10 send-offer-button js--triggerAnimation" type="button">
-                  <strong>{btn_title}</strong>
-                </button>
+                <div className="opinion-box-content projects-opinion-2 copywriter-prawo-text">
+                  <p className="theme-desc projects-client-opinion-p text-center">
+                    {title}
+                  </p>
+                  <div className="opinion-box-text copywriter-opinion-box-text">
+                    {description}
+                  </div>
+                  <button
+                    onClick={() => setIsModal(true)}
+                    className="download-catalog-button projects-opinion-button copywriter-blue-background copywriter-new-btn copywriter-standard-mt-10 send-offer-button js--triggerAnimation"
+                    type="button"
+                  >
+                    <strong>{btn_title}</strong>
+                  </button>
+                </div>
+                <div className="clearfix"></div>
               </div>
-              <div className="clearfix"></div>
             </div>
+            <div className="col-md-12 projects-empty-opinion-box"></div>
           </div>
-          <div className="col-md-12 projects-empty-opinion-box"></div>
         </div>
-      </div>
-    </section>
-    <ModalForm isOpen={isModal} onClose={handleModalClose}/>
+      </section>
+      <ModalForm isOpen={isModal} onClose={handleModalClose} />
     </div>
-  )
-}
+  );
+};
 
-export default OptionBox2
+export default OptionBox2;
