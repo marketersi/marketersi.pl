c12b613e1c07e2db5af1126e33428188bb56b544
chiranjivDev <chiranjivmohanty@resiliencesoft.com>
2024-08-01 15:34:22 +0530

    fix points from the sheet

--- src/components/organisms/marketing-consulting/customers-review/CustomersReview.tsx
+++ src/components/organisms/marketing-consulting/customers-review/CustomersReview.tsx
@@ -1,34 +1,27 @@
-import React from "react";
-import "./customers-review.css";
-import { Container } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import React from 'react';
+import './customers-review.css';
+import { Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
 const CustomersReview = () => {
   const { isLoading, screenData } = useSelector((state) => state.consulting);
 
   const { CustomeReview } = screenData || {};
   const { image_url, subtitle, title } = CustomeReview || {};
-  
 
   return (
     <Container className="cr_sec">
       <div className="cr_header">
-        <h2>
-          {title}
-        </h2>
+        <h2>{title}</h2>
         <p>
-          <em>
-            {subtitle}
-          </em>
+          <em>{subtitle}</em>
         </p>
       </div>
-      <div>
-        <img
-          src={image_url}
-          alt="reviews collage"
-          className="w-100"
-        />
-      </div>
+
+      {/* to be deleted */}
+      {/* <div>
+        <img src={image_url} alt="reviews collage" className="w-100" />
+      </div> */}
     </Container>
   );
 };
--- src/components/organisms/strategia-marketingowa/Branding.tsx
+++ src/components/organisms/strategia-marketingowa/Branding.tsx
@@ -1,27 +1,25 @@
-import Image from "next/image";
-import { useSelector } from "react-redux";
+import Image from 'next/image';
+import { useSelector } from 'react-redux';
 
 export default function Branding() {
-
-const {isLoading, screenData} = useSelector((state)  => state.strategy)
-const {Branding} =screenData;
+  const { isLoading, screenData } = useSelector((state) => state.strategy);
+  const { Branding } = screenData;
   return (
     <div className="Branding content">
       <div className="Branding_Container full">
         <div className="Branding_Content_Container">
           <div className="Branding_Title">{Branding?.title}</div>
-          <div className="Branding_Text">
-            {Branding?.description}
-          </div>
+          <div className="Branding_Text">{Branding?.description}</div>
         </div>
-        <div className="Branding_Image">
+        {/* to be deleted */}
+        {/* <div className="Branding_Image">
           <Image
             src={Branding?.image}
             width={635}
             height={371}
             alt="BrandingImage"
           />
-        </div>
+        </div> */}
       </div>
     </div>
   );
