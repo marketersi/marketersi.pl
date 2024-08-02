a58ed2d88198d63476534cca6552de4f58e97da8
chiranjivDev <chiranjivmohanty@resiliencesoft.com>
2024-08-02 12:53:10 +0530

    change base URL

--- src/services/httpServices.ts
+++ src/services/httpServices.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://marketersi.phpteam.in/',
+  // baseURL: 'https://marketersi.phpteam.in/',
+  baseURL: 'https://marketersi-new.phpteam.in',
   headers: {},
 });
