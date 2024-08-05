77209a85a8b07714b81e40c14bde0153b61a1187
chiranjivDev <chiranjivmohanty@resiliencesoft.com>
2024-08-05 13:46:30 +0530

    add initial lottie logo with black and white

--- src/components/molecules/LottieAnimation.jsx
+++ src/components/molecules/LottieAnimation.jsx
@@ -1,15 +1,67 @@
+// import { useState, useEffect } from 'react';
+// import Lottie from 'react-lottie';
+// import animationData from './data.json';
+// import blackLogo from './blackLogo.json';
+// import whiteLogo from './whiteLogo.json';
+
+// const LottieAnimation = () => {
+//   const [isPaused, setIsPaused] = useState(false);
+//   const [direction, setDirection] = useState(1);
+
+//   const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: blackLogo,
+//     rendererSettings: {
+//       preserveAspectRatio: 'xMidYMid slice',
+//     },
+//   };
+
+//   const handleMouseEnter = () => {
+//     setDirection(1);
+//     setIsPaused(false);
+//   };
+
+//   const handleMouseLeave = () => {
+//     setDirection(-1);
+//     setIsPaused(false);
+//   };
+
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setIsPaused(true); // Pause the animation after it plays initially
+//     }, 3000); // Adjust the time based on your animation length
+//   }, []);
+
+//   return (
+//     <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+//       <Lottie
+//         options={defaultOptions}
+//         height={150}
+//         width={400}
+//         isPaused={isPaused}
+//         direction={direction}
+//       />
+//     </div>
+//   );
+// };
+
+// export default LottieAnimation;
+
 import { useState, useEffect } from 'react';
 import Lottie from 'react-lottie';
-import animationData from './data.json';
+import blackLogo from './blackLogo.json';
+import whiteLogo from './whiteLogo.json';
 
 const LottieAnimation = () => {
   const [isPaused, setIsPaused] = useState(false);
   const [direction, setDirection] = useState(1);
+  const [animationData, setAnimationData] = useState(whiteLogo); // State for animation data
 
   const defaultOptions = {
     loop: true,
     autoplay: true,
-    animationData: animationData,
+    animationData, // Use the current animation data
     rendererSettings: {
       preserveAspectRatio: 'xMidYMid slice',
     },
@@ -18,17 +70,21 @@ const LottieAnimation = () => {
   const handleMouseEnter = () => {
     setDirection(1);
     setIsPaused(false);
+    setAnimationData(whiteLogo); // Change to white logo on hover
   };
 
   const handleMouseLeave = () => {
     setDirection(-1);
     setIsPaused(false);
+    setAnimationData(blackLogo); // Change back to black logo on mouse leave
   };
 
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setIsPaused(true); // Pause the animation after it plays initially
     }, 3000); // Adjust the time based on your animation length
+
+    return () => clearTimeout(timer); // Cleanup the timer on unmount
   }, []);
 
   return (
