2160c35659c369c9b867750a693bc9c7a87b466e
premsahu88 <103987763+premsahu88@users.noreply.github.com>
2024-09-26 15:13:10 +0530

    update flip catd design on ux screen

--- src/components/organisms/ux/Market.tsx
+++ src/components/organisms/ux/Market.tsx
@@ -6,61 +6,61 @@ const intervalDuration = 5000;
 const typingSpeed = 150;
 
 function Market() {
-  const { screenData } = useSelector((state) => state.ux);
-  const { cardOne, cardTwo, cardThree, cardFour } = screenData.market || {};
+  // const { screenData } = useSelector((state) => state.ux);
+  // const { cardOne, cardTwo, cardThree, cardFour } = screenData.market || {};
   
-  const cards = [
-    { title: cardOne?.title, color: "#FF5000" },
-    { title: cardTwo?.title, color: "#1EC19B" },
-    { title: cardThree?.title, color: "#EE8AA1" },
-    { title: cardFour?.title, color: "#FFC700" }
-  ];
+  // const cards = [
+  //   { title: cardOne?.title, color: "#FF5000" },
+  //   { title: cardTwo?.title, color: "#1EC19B" },
+  //   { title: cardThree?.title, color: "#EE8AA1" },
+  //   { title: cardFour?.title, color: "#FFC700" }
+  // ];
 
-  const [currentCardIndex, setCurrentCardIndex] = useState(0);
-  const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
-  const [sentence, setSentence] = useState("");
-  const [color, setColor] = useState("#FF5000");
+  // const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  // const [currentLetterIndex, setCurrentLetterIndex] = useState(0);
+  // const [sentence, setSentence] = useState("");
+  // const [color, setColor] = useState("#FF5000");
 
-  useEffect(() => {
-    if (cards[currentCardIndex]?.title) {
-      setSentence(cards[currentCardIndex].title);
-      setColor(cards[currentCardIndex].color);
-      setCurrentLetterIndex(0);
-    }
-  }, [currentCardIndex]);
+  // useEffect(() => {
+  //   if (cards[currentCardIndex]?.title) {
+  //     setSentence(cards[currentCardIndex].title);
+  //     setColor(cards[currentCardIndex].color);
+  //     setCurrentLetterIndex(0);
+  //   }
+  // }, [currentCardIndex]);
 
-  useEffect(() => {
-    let typingTimeout;
+  // useEffect(() => {
+  //   let typingTimeout;
 
-    const type = () => {
-      setCurrentLetterIndex((prevIndex) => prevIndex + 1);
-      typingTimeout = setTimeout(type, typingSpeed);
-    };
+  //   const type = () => {
+  //     setCurrentLetterIndex((prevIndex) => prevIndex + 1);
+  //     typingTimeout = setTimeout(type, typingSpeed);
+  //   };
 
-    type();
+  //   type();
 
-    return () => {
-      clearTimeout(typingTimeout);
-    };
-  }, [sentence]);
+  //   return () => {
+  //     clearTimeout(typingTimeout);
+  //   };
+  // }, [sentence]);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentCardIndex((prevIndex) => (prevIndex + 1) % cards.length);
-    }, intervalDuration);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrentCardIndex((prevIndex) => (prevIndex + 1) % cards.length);
+  //   }, intervalDuration);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, [cards.length]);
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, [cards.length]);
 
   return (
     <>
-      <div className="TypeMe typeme1 text-center projects-custom-typed-text">
+      {/* <div className="TypeMe typeme1 text-center projects-custom-typed-text">
         <span className="typed-cursor active" style={{ color: color }}>
           {sentence.substring(0, currentLetterIndex)}|
         </span>
-      </div>
+      </div> */}
       <Flip />
       
     </>
--- src/components/organisms/ux/flip/FlipCard.tsx
+++ src/components/organisms/ux/flip/FlipCard.tsx
@@ -4,73 +4,80 @@ import { useState, useEffect } from "react";
 
 const FlipCard = ({
     frontImageUrl,
-    backImageUrl,
-    backTopImg,
+    // backImageUrl,
+    // backTopImg,
     name,
-    textColor,
-    bgColor,
-    isFlipping,
-    typingText,
+    // textColor,
+    // bgColor,
+    // isFlipping,
+    // typingText,
   }) => {
-    const [isFlipped, setIsFlipped] = useState(false);
-    const [animateImage, setAnimateImage] = useState(false);
+    // const [isFlipped, setIsFlipped] = useState(false);
+    // const [animateImage, setAnimateImage] = useState(false);
   
-    useEffect(() => {
-      if (isFlipping) {
-        const timeoutId = setTimeout(() => {
-          setIsFlipped(true);
-        }, 500); 
+    // useEffect(() => {
+    //   if (isFlipping) {
+    //     const timeoutId = setTimeout(() => {
+    //       setIsFlipped(true);
+    //     }, 500); 
   
-        return () => clearTimeout(timeoutId);
-      } else {
-        setIsFlipped(false);
-        setAnimateImage(true); 
+    //     return () => clearTimeout(timeoutId);
+    //   } else {
+    //     setIsFlipped(false);
+    //     setAnimateImage(true); 
   
-        const animationTimeoutId = setTimeout(() => {
-          setAnimateImage(false); 
-        }, 500);
+    //     const animationTimeoutId = setTimeout(() => {
+    //       setAnimateImage(false); 
+    //     }, 500);
   
-        return () => clearTimeout(animationTimeoutId);
-      }
-    }, [isFlipping]);
+    //     return () => clearTimeout(animationTimeoutId);
+    //   }
+    // }, [isFlipping]);
   
     return (
+      // <div className="card">
+      //   <div className={`innerCard ${isFlipped ? "isFlipped" : ""}`}>
+      //     <div className="front" style={{ backgroundColor: bgColor }}>
+      //       <div className="imageContainer">
+      //         <Image
+      //           src={frontImageUrl}
+      //           alt={name}
+      //           layout="fill"
+      //           objectFit="cover"
+      //           className={`image ${animateImage ? "animate" : ""}`}
+      //         />
+      //       </div>
+      //       <div className="textContainer">
+      //         <h3 className="name" style={{ color: textColor }}>
+      //           {name}
+      //         </h3>
+      //       </div>
+      //     </div>
+      //     <div className="back" style={{ backgroundColor: bgColor }}>
+      //       <div
+      //         className={`backTopImg ${isFlipped ? "isFlipped" : "notFlipped"}`}
+      //       >
+      //         <Image src={backTopImg} alt={`${name} top image`} layout="fill" />
+      //       </div>
+      //       <div className="imageContainer">
+      //         <Image
+      //           src={backImageUrl}
+      //           alt={name}
+      //           layout="fill"
+      //           objectFit="cover"
+      //           className="image"
+      //         />
+      //       </div>
+      //       <h3 className="typingEffect">{typingText}</h3>
+      //     </div>
+      //   </div>
+      // </div>
       <div className="card">
-        <div className={`innerCard ${isFlipped ? "isFlipped" : ""}`}>
-          <div className="front" style={{ backgroundColor: bgColor }}>
-            <div className="imageContainer">
-              <Image
+         <Image
                 src={frontImageUrl}
                 alt={name}
                 layout="fill"
-                objectFit="cover"
-                className={`image ${animateImage ? "animate" : ""}`}
               />
-            </div>
-            <div className="textContainer">
-              <h3 className="name" style={{ color: textColor }}>
-                {name}
-              </h3>
-            </div>
-          </div>
-          <div className="back" style={{ backgroundColor: bgColor }}>
-            <div
-              className={`backTopImg ${isFlipped ? "isFlipped" : "notFlipped"}`}
-            >
-              <Image src={backTopImg} alt={`${name} top image`} layout="fill" />
-            </div>
-            <div className="imageContainer">
-              <Image
-                src={backImageUrl}
-                alt={name}
-                layout="fill"
-                objectFit="cover"
-                className="image"
-              />
-            </div>
-            <h3 className="typingEffect">{typingText}</h3>
-          </div>
-        </div>
       </div>
     );
   };
