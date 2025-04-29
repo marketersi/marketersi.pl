a38ae341154a2d62056bcb742dbe113d8f4fd584
Abdul Basit <ayushipareek563@gmail.com>
2025-04-29 12:47:01 +0530

    sheet-4th-issue

--- src/components/organisms/zamow/zamow-hero/questions/Question2.tsx
+++ src/components/organisms/zamow/zamow-hero/questions/Question2.tsx
@@ -22,7 +22,8 @@ const Question2 = ({ handleNext }) => {
 
   return (
     <div className="zh_question">
-      <h2>Nazwa Twojej firmy?</h2> 
+      {/* <h2>Nazwa Twojej firmy?</h2>  */}
+       <h2>Jak masz na nazwisko?</h2>  
       <textarea
         rows={1}
         placeholder="Wpisz odpowiedź tutaj"
