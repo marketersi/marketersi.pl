248814f88e6b912807132488164b371b2dd8a297
Ayushigithub <ayushipareek563@gmail.com>
2025-06-13 12:29:01 +0530

    chnages13june

--- src/components/organisms/professional-video-recording/hero/Hero.tsx
+++ src/components/organisms/professional-video-recording/hero/Hero.tsx
@@ -159,6 +159,7 @@ const Hero = () => {
       className={style.heroSection}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
+      style={{ position: "relative" }}
     >
       <ReactPlayer
         ref={playerRef}
@@ -189,37 +190,31 @@ const Hero = () => {
         }}
       />
 
+      {/* ✅ Correct overlay image on bottom-left */}
       {!isPlaying && (
         <div
           onClick={handlePlayVideo}
           style={{
             position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            background: "rgba(0,0,0,0.6)",
-            color: "#fff",
-            padding: "10px 20px",
-            borderRadius: "8px",
+            bottom: "20px",
+            left: "20px",
             cursor: "pointer",
-            display: "flex",
-            alignItems: "center",
-            fontSize: "16px",
             zIndex: 10,
           }}
         >
-          <svg
-            viewBox="0 0 256 256"
-            fill="white"
-            xmlns="http://www.w3.org/2000/svg"
-            style={{ height: "24px", width: "24px", marginRight: "10px" }}
-          >
-            <path d="M240 128C240.007 130.716 239.31 133.388 237.978 135.756C236.647 138.123 234.725 140.105 232.4 141.51L88.32 229.65C85.8909 231.138 83.1087 231.95 80.2608 232.002C77.4129 232.055 74.6025 231.347 72.12 229.95C69.6611 228.575 67.6128 226.57 66.1856 224.141C64.7585 221.712 64.0041 218.947 64 216.13V39.8701C64.0041 37.053 64.7585 34.2877 66.1856 31.8588C67.6128 29.4299 69.6611 27.4249 72.12 26.0501C74.6025 24.6536 77.4129 23.9451 80.2608 23.9979C83.1087 24.0506 85.8909 24.8626 88.32 26.3501L232.4 114.49C234.725 115.895 236.647 117.877 237.978 120.245C239.31 122.612 240.007 125.284 240 128Z" />
-          </svg>
-          Kliknij, by obejrzeć z dźwiękiem
+          <img
+            src="/assets/images/Kliknij, aby obejrzec z dziekiem.jpg"
+            alt="Kliknij, aby obejrzeć z dźwiękiem"
+            style={{
+              width: "130px",  // You can adjust size here if needed
+              height: "auto",
+              maxWidth: "100%",
+            }}
+          />
         </div>
       )}
 
+      {/* Pause button if hovered & playing */}
       {isPlaying && hovered && (
         <div
           onClick={() => setIsPlaying(false)}
@@ -249,3 +244,4 @@ const Hero = () => {
 };
 
 export default Hero;
+
